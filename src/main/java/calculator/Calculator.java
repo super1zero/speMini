2039c7eecb1a67ea364be9014b7c8d44abbb83dc
super1zero <koustubhtadanki1@gmail.com>
2022-04-17 18:29:55 +0530

    added log file

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,9 +1,12 @@
 package calculator;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.lang.Math;
 public class Calculator {
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
     public double SquareRoot(int x)
     {
+        logger.info("Square root of "+x+"\t"+ Math.sqrt(x));
         return Math.sqrt(x);
     }
     public double factorial(int x)
@@ -13,16 +16,19 @@ public class Calculator {
         {
             fac = fac*i;
         }
+        logger.info("Factorial of "+x+"\t"+ fac);
         return fac;
     }
 
     public double log(int x)
     {
+        logger.info("Logarithm of "+x+"\t"+ Math.log(x));
         return Math.log(x);
     }
 
     public double power(int x , int y)
     {
+        logger.info(x+"Power"+y+"\t"+ Math.pow(x, y));
         return Math.pow(x, y);
     }
 }
